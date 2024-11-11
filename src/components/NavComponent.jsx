0eb19c1db77890abe3fb7c8b1b4cbfc4350d3a1e
AflahAhamed00 <aflahahamed777@gmail.com>
2024-11-11 16:57:20 +0530

    updated

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -4,9 +4,12 @@ import gymshark from "../../public/gymshark-icon.svg";
 import NavItem from "./NavItem";
 import CartButton from "./CartButton";
 import Link from "next/link";
-import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 const NavComponent = memo(() => {
+  const router = useRouter();
+
   return (
     <header className="bg-white w-screen">
       <div className="flex fixed top-0 left-0 justify-between items-center w-full py-4 bg-white z-10">
@@ -18,11 +21,14 @@ const NavComponent = memo(() => {
           <CartButton />
           <div className="mr-10">
             <SignedOut>
-              <SignInButton>
-                <button className="px-2 py-2 rounded xl bg-gray-200 ">
-                  Sign In
-                </button>
-              </SignInButton>
+              <button
+                className="px-2 py-2 rounded xl bg-gray-200 "
+                onClick={() => {
+                  router.push("/sign-in");
+                }}
+              >
+                Sign In
+              </button>
             </SignedOut>
             <SignedIn>
               <UserButton />
