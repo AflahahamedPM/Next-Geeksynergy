f8a327efbba6c0834f2af87647f8f8b72676196f
AflahAhamed00 <aflahahamed777@gmail.com>
2024-11-13 10:56:38 +0530

    updated

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,13 +1,37 @@
 export default function sitemap() {
     return [
       {
-        url: 'https://next-geeksynergy.vercel.app/',
+        url: 'https://next-geeksynergy.vercel.app',
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1,
       },
       {
         url: 'https://next-geeksynergy.vercel.app/product/01',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://next-geeksynergy.vercel.app/product/02',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://next-geeksynergy.vercel.app/product/03',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://next-geeksynergy.vercel.app/product/04',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://next-geeksynergy.vercel.app/product/04',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.8,
