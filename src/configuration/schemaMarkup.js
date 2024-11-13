384f5c28d92fc53c8a4b11c31c5f9cd7083ff32c
AflahAhamed00 <aflahahamed777@gmail.com>
2024-11-13 10:26:26 +0530

    updated

--- src/configuration/schemaMarkup.js
+++ src/configuration/schemaMarkup.js
@@ -1,30 +1,38 @@
 import { cardDetails } from "../constants/CardDetails";
 
-const schemaMarkup = cardDetails.map((product)=>(
+const schemaMarkup = cardDetails.map((product) => {
+  const imageUrls = [
+    product.imageUrl.src,
+    product.subImage1.src,
+    product.subImage2.src,
+  ];
+  
+  return (
     <script type="application/ld+json" key={product.id}>
-    {JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "Product",
-      "name": product.title,
-      "description": product.description,
-      "category":product.category,
-      "brand": {
-        "@type": "Brand",
-        "name": product.brand
-      },
-      "url":"https://next-geeksynergy.vercel.app",
-      "image":[product.imageUrl, product.subImage1, product.subImage2],
-      "offers": {
-        "@type": "Offer",
-        "name":product.title,
-        "url": `https://next-geeksynergy.vercel.app/product/${product.id}`,
-        "priceCurrency": "USD",
-        "price": product.price,
-        "priceValidUntill":"2025-05-31",
-        "availability":  "https://schema.org/InStock"
-      }
-    })}
-  </script>
-))
+      {JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Product",
+        name: product.title,
+        description: product.description,
+        category: product.category,
+        brand: {
+          "@type": "Brand",
+          name: product.brand,
+        },
+        url: "https://next-geeksynergy.vercel.app",
+        image: imageUrls,
+        offers: {
+          "@type": "Offer",
+          name: product.title,
+          url: `https://next-geeksynergy.vercel.app/product/${product.id}`,
+          priceCurrency: "USD",
+          price: product.price,
+          priceValidUntill: "2025-05-31",
+          availability: "https://schema.org/InStock",
+        },
+      })}
+    </script>
+  );
+});
 
-export default schemaMarkup
+export default schemaMarkup;
